c1b53e26c3ed20378e7728c205de3b3c1647a1ff
etud user <etud@port-hp-elitebook650G9-23>
2023-12-13 10:51:02 +0100

    update solver 3

--- src/solver3.py
+++ src/solver3.py
@@ -10,22 +10,27 @@ class GolferConstraintSolver:
         self.rencontres = {i: set() for i in range(self.P*self.G)}
         self.constraints = []
         self.consistence = True
+        self.backtrack_domains = [[set(range(G*P)) for _ in range(G)]for _ in range(W)]
+        #self.backtrack_schedule = copy.deepcopy(self.schedule)
 
-        self.backtrack_domains = copy.deepcopy(self.domains)
-        self.backtrack_schedule = copy.deepcopy(self.schedule)
-
-    def __str__(self) -> str:
+    def printDomains(self):
         result = ""
         for w in range(self.W):
             result += str(self.domains[w][:]) + "\n"
-        return result
+        print(result)
+    
+    def printSchedule(self):
+        result = ""
+        for w in range(self.W):
+            result += str(self.schedule[w][:]) + "\n"
+        print(result)
 
     def add_constraint(self, constraint):
         self.constraints.append(constraint)
 
     def propagate_constraints(self):
         print("Domaines initiaux:\n")
-        print(self)
+        self.printDomains()
         for constraint in self.constraints:
             constraint.propagate(self.domains)
         
@@ -33,30 +38,27 @@ class GolferConstraintSolver:
         # reduction domaine
         print("W: "+str(self.W)+"\nG: "+str(self.G)+"\nP: "+str(self.P)+"\n")
         self.propagate_constraints()
-
-        # Met a jour la liste des rencontres pour les matchs deja fixé (de par les contraintes de symetrie)
-        for i in range(0,self.G*self.P):
-            for week in range(0,self.W):
-                for group in range(0,self.G):
-                    if i in self.schedule[week][group]:
-                        for player in self.schedule[week][group]:
-                            if player != i:
-                                self.rencontres[i].add(player)
+        self.backtrack_domains = copy.deepcopy(self.domains)
+ 
+        res_final=True
+        self.printDomains()
+        if self.consistence:
+            res_final=self.forwardCheking(0,0,0)
+            if res_final:
+                print("Solution trouve:\n")
+                self.printSchedule()
+            else:
+                print("Aucune solution trouve:\n")
+        else:
+            print("Pas de soluition")   
 
         
-        if self.consistence:
-            self.forwardCheking(1,0,1)
-
-        print("Solution:\n")
-        result = ""
-        for w in range(self.W):
-            result += str(self.schedule[w][:]) + "\n"
-        print(result)
 
     
-    def updateDomains(self,w,g,p):
+    def updateDomains(self):
 
-
+        self.domains = copy.deepcopy(self.backtrack_domains) 
+        # contrainte de la semaine
         for week in range(1,self.W):
             for group in range(0,self.G):
                 for player in self.schedule[week][group]:
@@ -64,10 +66,7 @@ class GolferConstraintSolver:
                         if group2 != group:
                             self.domains[week][group2] -= {player}
 
-        '''
-        for group in range(g+1,self.G):
-            self.domains[w][group]-={p}
-        '''
+        # Maj de la liste des rencontres
         self.rencontres = {i: set() for i in range(self.P*self.G)}
         for i in range(0,self.G*self.P):
             for week in range(0,self.W):
@@ -77,6 +76,7 @@ class GolferConstraintSolver:
                             if player != i:
                                 self.rencontres[i].add(player)
 
+        # contrainte unicite des rencontres
         for player in range(0,self.G*self.P):
             for week in range(1,self.W):
                 for group in range(0,self.G):
@@ -86,68 +86,55 @@ class GolferConstraintSolver:
                             to_remove-={player2}
                         self.domains[week][group]-=to_remove
 
-
     def forwardCheking(self,week,group,player):
         
         print(" ---------- Forward Cheking ",week," ",group," ",player, "----------\n")
-        
-        if week == self.W and group == self.G and player == self.P:
+        self.printSchedule()
+    
+        if week == self.W :
             return True  # La solution est trouvée
         
         if group < self.G-1 and player == self.P:
-            return self.forwardCheking(week, group+1, 1)  # Passage au groupe suivant
+            return self.forwardCheking(week, group+1, 0)  # Passage au groupe suivant
 
         if group == self.G-1 and player == self.P:
-            return self.forwardCheking(week + 1, 0,1)  # Passage à la semaine suivante
+            return self.forwardCheking(week + 1, 0,0)  # Passage à la semaine suivante
+
         
-
-        print(self)
-        # Remplis le schedule en respectant les contrainte et en mettant a jour les domaines
-        # Le tableau peut ne pas etre complet
-        
-
-        nb_joueur_affecte = len(self.schedule[week][group])
-
-
-        result = ""
-        for w in range(self.W):
-            result += str(self.schedule[w][:]) + "\n"
-        print(result)
-
         if player < len(self.schedule[week][group]):
-            self.forwardCheking(week, group ,player+1)
+                return self.forwardCheking(week, group ,player+1) # Passage au joueur suivant
+        
+        # recupere le dernier joueur placer dans le group
+        last_player=-1
+        for last in self.schedule[week][group]:
+            last_player = last
 
-        for p in list(self.domains[week][group])[player:]:
-            #if len(self.domains[week][group])>=nb_joueur_affecte+nb_player_manquant:
-            if self.isConsistent(week,group,p):
-                
-            
-                for p2 in self.schedule[week][group]:
-                    self.rencontres[p].add(p2)
-                    self.rencontres[p2].add(p)
-                self.schedule[week][group].append(p)
-                self.updateDomains(week,group,p)
-
-                print("Choix player: ",p)
-                print("Domains update:\n",self)
-
-                if self.forwardCheking(week, group , player+1):  # Récursion
+        # Paarcour des joueur dispo dans le domaine et pas deja place et superieur au precedent joueur place (symetrie)
+        for p in list(self.domains[week][group]):
+            if p not in self.schedule[week][group] and p>last_player:
+                # Si consistent on l ajoute et on met a jour les domaines
+                if self.isConsistent(week,group,p):
+                    for p2 in self.schedule[week][group]:
+                        self.rencontres[p].add(p2)
+                        self.rencontres[p2].add(p)
+                    self.schedule[week][group].append(p)
+                    self.updateDomains()
+                    # On passe au joueur suivant par recursivité
+                    if self.forwardCheking(week, group , player+1):  # Récursion
                         return True
-                
-                self.schedule[week][group].remove(p)
-                self.updateDomains(week, group, p)
-
+                    
+                    # Si inconsistence dans les prochain joueurs on suprime et on met a jour les domaines
+                    self.schedule[week][group].remove(p)
+                    self.updateDomains()
             
         return False  # Aucune assignation valide pour cette variable
             
 
     def isConsistent(self,w,g,p):
-        domains_copy = copy.deepcopy(self.domains)
+        # on stimule la maj du schedule et des domaines. Si certain domaines sont plut petit que P alors inconsistent
+        domains_copy = copy.deepcopy(self.backtrack_domains)
         schedule_copy = copy.deepcopy(self.schedule)
-        rencontres_copy = copy.deepcopy(self.rencontres)
-        for p2 in schedule_copy[w][g]:
-            rencontres_copy[p].add(p2)
-            rencontres_copy[p2].add(p)
+        
         schedule_copy[w][g].append(p)
         
         for week in range(1,self.W):
@@ -202,12 +189,12 @@ class PPlayerPerGroupConstraint:
                     res = False
 
         print("Si cardinalite d un groupe == P, alors modification des domaine des autres groupes:\n")
-        print(self.golfer_solver)
+        self.golfer_solver.printDomains()
         print("Check cardinalité consistence = "+str(res)+"\n")
         if res == False:
             self.golfer_solver.consistence=False
-            print("Pas de soluition")
-    
+
+
 class Week_constraint:
     def __init__(self, golfer_solver):
         self.golfer_solver = golfer_solver
@@ -223,7 +210,7 @@ class Week_constraint:
         print("Check week consistence = "+str(res)+"\n")
         if res == False:
             self.golfer_solver.consistence=False
-            print("Pas de soluition")   
+            
 
 class SymetrieFixSem1:
     def __init__(self, golfer_solver,params=None):
@@ -235,7 +222,7 @@ class SymetrieFixSem1:
             self.golfer_solver.schedule[0][group] = [i for i in range(group * self.golfer_solver.P, (group + 1) * self.golfer_solver.P)]
             domains[0][group] = set((range(group * self.golfer_solver.P, (group + 1) * self.golfer_solver.P)))
         print("Symetrie premiere semaine fix:\n")
-        print(self.golfer_solver)
+        self.golfer_solver.printDomains()
 
 class SymetrieFixFirstPlayer:
     def __init__(self, golfer_solver):
@@ -254,41 +241,47 @@ class SymetrieFixFirstPlayer:
                 else:
                     domains[week][group]-=group1
         print("Symetrie premier joueur de chaque groupe fix:\n")
-        print(self.golfer_solver)
+        self.golfer_solver.printDomains()
 
 class SymetrieMinMaxFirstGroup:
     def __init__(self, golfer_solver):
         self.golfer_solver = golfer_solver
 
     def propagate(self, domains):
+        res=True
         for w2 in range(self.golfer_solver.W-1,1,-1):
-            max_w2=max(domains[w2][0])
-            for w1 in range(w2-1,0,-1):
-                max_to_remove=set()
-                for i in domains[w1][0]:
-                    if i >= max_w2:
-                        max_to_remove.add(i)
-                domains[w1][0]-=max_to_remove
-
+            if len(domains[w2][0]) > 1:
+                max_w2=max(domains[w2][0])
+                for w1 in range(w2-1,0,-1):
+                    max_to_remove=set()
+                    for i in domains[w1][0]:
+                        if i >= max_w2:
+                            max_to_remove.add(i)
+                    domains[w1][0]-=max_to_remove
+            else:
+                res=False
+        
         for w1 in range(1,self.golfer_solver.W-1):
-            copy_g1 = copy.deepcopy(domains[w1][0])
-            copy_g1-={0}
-            min_w1=min(copy_g1)
-            for w2 in range(w1+1,self.golfer_solver.W):
-                min_to_remove=set()
-                for i in domains[w2][0]:
-                    if i <= min_w1 and i!=0:
-                        min_to_remove.add(i)
-                domains[w2][0]-=min_to_remove
+            if len(domains[w1][0]) > 1:
+                copy_g1 = copy.deepcopy(domains[w1][0])
+                copy_g1-={0}
+                min_w1=min(copy_g1)
+                for w2 in range(w1+1,self.golfer_solver.W):
+                    min_to_remove=set()
+                    for i in domains[w2][0]:
+                        if i <= min_w1 and i!=0:
+                            min_to_remove.add(i)
+                    domains[w2][0]-=min_to_remove
+            else:
+                res=False
         print("Symetrie min(w1)<min(w2) et max(w1)<max(w2)\n")
-        print(self.golfer_solver)
+        print("Check min_max = "+str(res)+"\n")
+        if res == False:
+            self.golfer_solver.consistence=False
+        self.golfer_solver.printDomains()
 
 
-
-
-# 4 2 2 -> impossible
-
-solver = GolferConstraintSolver(W=4, G=3, P=3)
+solver = GolferConstraintSolver(W=4, G=4, P=4)
 
 symetrie_fix_sem_1 = SymetrieFixSem1(solver)
 symetrie_fix_first_player = SymetrieFixFirstPlayer(solver)
