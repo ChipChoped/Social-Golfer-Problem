f89f76e583b2f93f8b6a02fc3c29cfdcf6a615c6
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-12-14 13:26:40 +0100

    Removed condition on instance parameters

--- src/instances_generator.sh
+++ src/instances_generator.sh
@@ -11,36 +11,34 @@ echo "weeks,groups,participants,model,symmetry_breaking,all_solutions,n_solution
 for w in $(seq 1 "$max") ; do
   for g in $(seq 1 "$w") ; do
     for p in $(seq 1 "$w") ; do
-      if [[ "$w" < $(( g*p )) ]] || { [[ "$g" == "$w" ]] && [[ "$p" == 1 ]]; } ; then
-        for m in {1..3} ; do
-          for s in false true ; do
-            for a in false true ; do
-              if [[ "$s" == true ]] && [[ "$a" == true ]] ; then
-                exec=$(python ./main.py -w "$w" -g "$g" -p "$p" -m "$m" -s -a -t "$t")
-              elif [[ "$s" == true ]] && [[ "$a" == false ]] ; then
-                exec=$(python ./main.py -w "$w" -g "$g" -p "$p" -m "$m" -s -t "$t")
-              elif [[ "$s" == false ]] && [[ "$a" == true ]] ; then
-                exec=$(python ./main.py -w "$w" -g "$g" -p "$p" -m "$m" -a -t "$t")
-              else
-                exec=$(python ./main.py -w "$w" -g "$g" -p "$p" -m "$m" -t "$t")
-              fi
+      for m in {1..3} ; do
+        for s in false true ; do
+          for a in false true ; do
+            if [[ "$s" == true ]] && [[ "$a" == true ]] ; then
+              exec=$(python ./main.py -w "$w" -g "$g" -p "$p" -m "$m" -s -a -t "$t")
+            elif [[ "$s" == true ]] && [[ "$a" == false ]] ; then
+              exec=$(python ./main.py -w "$w" -g "$g" -p "$p" -m "$m" -s -t "$t")
+            elif [[ "$s" == false ]] && [[ "$a" == true ]] ; then
+              exec=$(python ./main.py -w "$w" -g "$g" -p "$p" -m "$m" -a -t "$t")
+            else
+              exec=$(python ./main.py -w "$w" -g "$g" -p "$p" -m "$m" -t "$t")
+            fi
 
-              n_solutions=$(echo "$exec" | grep "solutions found:" | cut -d' ' -f5)
-              time=$(echo "$exec" | grep "time:" | cut -d' ' -f3)
-              timed_out=$(echo "$exec" | grep "Timeout reached")
+            n_solutions=$(echo "$exec" | grep "solutions found:" | cut -d' ' -f5)
+            time=$(echo "$exec" | grep "time:" | cut -d' ' -f3)
+            timed_out=$(echo "$exec" | grep "Timeout reached")
 
-              if [[ "$timed_out" == "Timeout reached" ]] ; then
-                timed_out=true
-              else
-                timed_out=false
-              fi
+            if [[ "$timed_out" == "Timeout reached" ]] ; then
+              timed_out=true
+            else
+              timed_out=false
+            fi
 
-              echo "$w $g $p $m $s $a $n_solutions $time $timed_out"
-              echo "$w,$g,$p,$m,$s,$a,$n_solutions,$time,$timed_out" >> "$output_file"
-            done
+            echo "$w $g $p $m $s $a $n_solutions $time $timed_out"
+            echo "$w,$g,$p,$m,$s,$a,$n_solutions,$time,$timed_out" >> "$output_file"
           done
         done
-      fi
+      done
     done
   done
 done
