9dd2a326b4de3c85ff99af31a6dac550a4164a8f
Chaïmaâ Touhami <76920840+ChipChoped@users.noreply.github.com>
2023-12-15 02:11:28 +0100

    Readaptation

--- src/solver_generator.sh
+++ src/solver_generator.sh
@@ -9,41 +9,55 @@ output_file="${output_dir}instances_solver_$max-w_$t-t_$date.csv"
 
 mkdir -p "$output_dir"
 
-echo "weeks,groups,participants,model,symmetry_breaking,all_solutions,n_solutions,time,timed_out" > "$output_file"
+echo "weeks,groups,participants,solution,time,timed_out,valid" > "$output_file"
 
 for w in $(seq 1 "$max") ; do
   for g in $(seq 1 "$w") ; do
     for p in $(seq 1 "$w") ; do
-      if [[ "$w" < $(( g*p )) ]] || { [[ "$g" == "$w" ]] && [[ "$p" == 1 ]]; } ; then
+#      if [[ "$w" < $(( g*p )) ]] || { [[ "$g" == "$w" ]] && [[ "$p" == 1 ]]; } ; then
         #for m in {1..3} ; do
           for s in true ; do
             #for a in true false ; do
               if [[ "$s" == true ]] && [[ "$a" == true ]] ; then
-                exec=$(python3 ./solver3.py -w "$w" -g "$g" -p "$p" -s -t "$t")
+                exec=$(python3 ./solver3.py -w "$w" -g "$g" -p "$p" -s -t "$t" -c)
               elif [[ "$s" == true ]] && [[ "$a" == false ]] ; then
-                exec=$(python3 ./solver3.py -w "$w" -g "$g" -p "$p" -s -t "$t")
+                exec=$(python3 ./solver3.py -w "$w" -g "$g" -p "$p" -s -t "$t" -c)
               elif [[ "$s" == false ]] && [[ "$a" == true ]] ; then
-                exec=$(python3 ./solver3.py -w "$w" -g "$g" -p "$p" -t "$t")
+                exec=$(python3 ./solver3.py -w "$w" -g "$g" -p "$p" -t "$t" -c)
               else
-                exec=$(python3 ./solver3.py -w "$w" -g "$g" -p "$p" -t "$t")
+                exec=$(python3 ./solver3.py -w "$w" -g "$g" -p "$p" -t "$t" -c)
               fi
 
-              n_solutions=$(echo "$exec" | grep "solutions:" | cut -d' ' -f4)
+              solution=$(echo "$exec" | grep "No solution found")
               time=$(echo "$exec" | grep "time:" | cut -d' ' -f3)
               timed_out=$(echo "$exec" | grep "Timeout reached")
+              valid=$(echo "$exec" | grep "is valid")
+
+              if [[ "$solution" == "No solution found" ]] ; then
+                solution=false
+              else
+                solution=true
+              fi
 
               if [[ "$timed_out" == "Timeout reached" ]] ; then
                 timed_out=true
+                solution=false
               else
                 timed_out=false
               fi
 
-              echo "$w $g $p $time $timed_out"
-              echo "$w,$g,$p,$time,$timed_out" >> "$output_file"
+              if [[ "$valid" == "The schedule is valid" ]] ; then
+                valid=true
+              else
+                valid=false
+              fi
+
+              echo "$w $g $p $solution $time $timed_out $valid"
+              echo "$w,$g,$p,$solution,$time,$timed_out,$valid" >> "$output_file"
             #done
           done
         #done
-      fi
+#      fi
     done
   done
 done
